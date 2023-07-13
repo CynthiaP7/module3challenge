90e8eec59a1ad02c4ca58194ab65caafcd3b8987
Cynthia P <cynthiads417@gmail.com>
2023-07-13 12:11:24 -0400

    created different var for password requirements

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,30 @@ var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
     var password = generatePassword();
-    var passwordText = document.quesrySelector("#password");
+    var passwordText = document.quesySelector("#password");
 
     passwordText.value = password;
 }
 
+function generatePassword() {
+    var uppercase = confirm ("Use uppercase chracters?");
+    var lowercase = confirm ("Use lowercase characters?");
+    var length = prompt ("Number of characters in the password (8-128)");
+    var numeric = confirm ("Use number characters?");
+    var specialCharacters = confirm ("Use special characters?");
+
+    if (uppercase === false && lowercase === false && numeric === false && specialCharacters === false) {
+        alert ("You must select one")
+        return
+    }
+
+    
+
+
+    return password
+}
+
+
+
+
+generateBtn.addEventListener("click", writePassword);
